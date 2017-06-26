890240b90c032874dfb3adf9e510010e3aaf5d5a
RadioFreeMoscow <spozguy@gmail.com>
2017-06-26 11:48:32 +1000

    Initial Committ
    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('.opensesame').click(showNav);
+
+    function showNav(){
+      event.preventDefault();
+      document.getElementById('sidenav').style.width="250px";
+
+    }
+
+
+
+});
